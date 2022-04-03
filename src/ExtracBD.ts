021fc3b1140a749e16c02687302e46c58d61dfb3
JessicaJoya <jessica.joyaq@gmail.com>
2022-04-03 18:51:00 -0500

    recupera informacion estatica con cheerio

--- src/ExtracBD.ts
+++ src/ExtracBD.ts
@@ -1,1 +1,43 @@
-console.log("hola mundo")
+// import axios from "axios";
+// import cheerio from "cheerio";
+// import mysql from "mysql";
+
+const axios = require ("axios");
+const cheerio = require('cheerio');
+
+// const getInfo = async () =>{
+//     const url="https://www.ambito.com/contenidos/dolar-informal.html"
+//     const {data}= await axios.get(url);
+//     const $ = cheerio.load(data)
+//     const ventaDolar =[];
+
+
+
+// }
+
+// getInfo();
+const fetchTitles = async () => {
+    try {
+           const url = 'https://www.ambito.com/contenidos/dolar.html';
+
+     const response = await axios.get(url);
+     const {data}= await axios.get(url);
+     const $ = cheerio.load(data)
+
+     const titles = [];
+   
+  
+    $('div.first > span').each((_idx, el) => {
+          const title = $(el).text()
+          titles.push(title)
+         });
+   
+     return titles;
+    } catch (error) {
+     throw error;
+    }
+   };
+   
+   fetchTitles().then((titles) => console.log(titles));
+   
+
