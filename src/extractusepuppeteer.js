54766f50658569ac62776b4770f8b043306164a2
JessicaJoya <jessica.joyaq@gmail.com>
2022-04-04 01:28:33 -0500

    get data with fetchValues

--- src/extractusepuppeteer.js
+++ src/extractusepuppeteer.js
@@ -1,24 +1,24 @@
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+// const puppeteer = require('puppeteer');
+// const cheerio = require('cheerio');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.ambito.com/contenidos/dolar.html');
-  await page.screenshot({ path: 'example.png' });
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto('https://www.ambito.com/contenidos/dolar-informal.html');
 
-  const pageData = await page.evaluate(()=>{
-       return{ pageData: document.documentElement.innerHTML,
-    };
-  });
-  var pageDataString  = JSON.stringify(pageData);
+// const dolarCompra = await page.evaluate(()=>{
+//            return{ compra: document.querySelector('div.first > span.data-compra').innerText,
+//         };
+//       });
 
-  console.log(pageData)
+// const dolarVenta = await page.evaluate(()=>{
+//         return{venta: document.querySelector('div.second > span.data-venta').innerText,
+//      };
+//    });
 
-  
-
-  await browser.close();
-})();
+// console.log(dolarCompra,dolarVenta)
+//   await browser.close();
+// })();
 
 
 
